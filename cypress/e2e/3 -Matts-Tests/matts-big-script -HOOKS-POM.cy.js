a1c9612f3b84f0c7459bb8fc37814da85b7e442f
matt brown <mathew.brown@bt.com>
2025-08-04 13:28:07 +0100

    Sam to review POM Full tests with Hooks

--- cypress/e2e/3 -Matts-Tests/matts-big-script -HOOKS-POM.cy.js
+++ cypress/e2e/3 -Matts-Tests/matts-big-script -HOOKS-POM.cy.js
@@ -1,13 +1,16 @@
 import "cypress-axe";
 import NBSHomepage from "../../support/page-objects/nbs-homepage";
 import DysonHomepage from "../../support/page-objects/dyson-homepage";
+import nbsHomepage from "../../support/page-objects/nbs-homepage";
 
 describe("Matts Dyson Homepage Regression Tests", () => {
   beforeEach(() => {
     NBSHomepage.visitNBSHomepage();
     NBSHomepage.acceptCookies();
-    NBSHomepage.searchFor("Dyson");
-    NBSHomepage.selectDysonResult();
+    nbsHomepage.searchFor("Dyson");
+    nbsHomepage.selectDysonResult();
+    })
+  
   });
 
   // 1 - Verifies that the URL is correct for Dyson homepage
@@ -15,110 +18,48 @@ describe("Matts Dyson Homepage Regression Tests", () => {
     DysonHomepage.verifyDysonPage();
   });
 
-  //     // 2- Verifies the contact number link is visible, has correct text, and correct tel: protocol
+  // 2- Verifies the contact number link is visible, has correct text, and correct tel: protocol
   it("2- verify telephone number is as expected", () => {
-    cy.get('a[href="tel:08003457788"]')
-      .should("be.visible") // Ensure the contact number is visible
-      .should("have.text", " 08003457788 "); // Ensure the text matches the expected number
-    // Additionally, verify the href uses the correct telephone protocol, ie tel:
-    cy.get('a[href="tel:08003457788"]').should(
-      "have.attr",
-      "href",
-      "tel:08003457788"
-    );
+    DysonHomepage.verifyContactNumber();
   });
 
   //3- I verify the H1 title on the page is as expected
   it("3- verify H1 title is as expected", () => {
-    cy.get("h1").should("contain.text", "Dyson");
+    DysonHomepage.verifyH1Title(); 
   });
 
-  //4- I verify the href attribute of the Source logo is as expected
+  // //4- I verify the href attribute of the Source logo is as expected
   it("4- verify the href attribute of the source logo is as expected", () => {
-    cy.get('a[href="https://source.thenbs.com/"]')
-      .should("be.visible") // Ensure the Source logo link is visible
-      .should("have.attr", "href", "https://source.thenbs.com/"); // Verify the href attribute is correct
+    DysonHomepage.verifySourceLogoHref();
 
-    cy.get('a[href="/"]')
-      .should("be.visible") // Ensure the Source logo link is visible
-      .should("have.attr", "href", "/"); // Verify the href attribute will be as expected "/"
   });
 
   //5- I verify the external manufacturer link attribute contains the correct url
   it("5- verify the external manufacturer link attribute contains the correct url", () => {
-    cy.get(
-      'a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]'
-    )
-      .should("be.visible", { timeout: 10000 }) // Ensure the external manufacturer link is visible
-      .should(
-        "have.attr",
-        "href",
-        "https://www.dyson.co.uk/commercial/overview/architects-designers"
-      ) // Verify the href attribute is correct
-      .should("have.text", " Website "); // Verify the link text is 'Website'
+    DysonHomepage.verifyWebsiteLink();
+
   });
 
   //6- I verify the contact manufacturer button shows the correct text
   it("6- verify the contact manufacturer button shows the correct text", () => {
-    cy.get(".contact-button > .mdc-button__label")
-      .should("be.visible") // Ensure the contact manufacturer button is visible
-      .should("contain.text", "Contact manufacturer"); // Verify the button text is as expected
+    DysonHomepage.verifyContactManufacturerButton();
+    
   });
 
   // 7- I run accessibility checks on the manufacturer homepage using AXE plugin and report results to console
   it("7- verify accessibility on manufacturer homepage using Axe plugin", () => {
-    // Logs accessibility violations to the Cypress test runner console using Axe.
-    cy.injectAxe();
-    cy.checkA11y(
-      null,
-      null,
-      (violations) => {
-        // Log the violations without failing the test
-        cy.task("log", violations);
-        violations.forEach((violation) => {
-          const nodes = Cypress.$(
-            violation.nodes.map((node) => node.target).join(",")
-          );
-          Cypress.log({
-            name: "a11y error!",
-            consoleProps: () => violation,
-            $el: nodes,
-            message: `[${violation.id}] ${violation.help} (${violation.nodes.length} nodes)`,
-          });
-        });
-      },
-      { timeout: 10000 }
-    );
+    DysonHomepage.verifyAccessibilityChecks();
+
   });
 
   // //8- I perform an api test and verify the response and content is as expected
   it("8- Verify API content and UI display", () => {
-    cy.request({
-      method: "GET",
-      url: "https://geolocation.onetrust.com/cookieconsentpub/v1/geo/location",
-    }).then((response) => {
-      expect(response.status).to.eq(200); // Verify the response status is 200
-      expect(JSON.stringify(response.body)).to.include("country"); // Checks for the word "country"
-      expect(JSON.stringify(response.body)).to.satisfy(
-        (str) => str.includes("GB") || str.includes("US")
-      ); // Checks for "GB" or "US"
+    DysonHomepage.verifyApiResponse();
 
-      cy.get(".mdc-button__label")
-        .should("be.visible")
-        .and("contain.text", "UK");
-    });
   });
 
   // //9- I verify the Dyson navigation bar has the correct tabs and expected links
   it("9- verify the Dyson navigation bar has the correct tabs and expected links", () => {
-    cy.get(".mat-mdc-tab-links")
-      .should("be.visible")
-      .should("contain.text", "Overview") // Ensure the first tab is 'Overview'
-      .and("contain.text", "Products") // Ensure the second tab is 'Products'
-      .and("contain.text", "Case studies");
+    DysonHomepage.verifyDysonNavigationBar();
 
-    cy.get(
-      '.mat-mdc-tab-links a[href="/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview"]'
-    ).should("be.visible"); // Ensure the 'Overview' tab link is visible
-  });
-});
+})
