d0f6ff18bac3ad21b2edf388a87fa6308ae7da5c
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-08-13 15:00:56 +0100

    removed incorrect fixture call

--- cypress/e2e/step-definitions/dyson-homepage.js
+++ cypress/e2e/step-definitions/dyson-homepage.js
@@ -6,69 +6,75 @@ const nbsHomepage = require("../../support/page-objects/nbs-homepage");
 
 // Import Cucumber preprocessor functions
 var {
-    Given,
-    Then,
-    Before,
+  Given,
+  Then,
+  Before,
 } = require("@badeball/cypress-cucumber-preprocessor");
 
 // Given step to visit NBSHomepage then visit the manufacturer home page
-Given('I navigate to the Dyson manufacturer homepage', () => {
-    cy.fixture('urls').then((urls) => {
-        nbsHomepage.visitURL(urls.nbsHomepage);
-    });
-    nbsHomepage.acceptCookies();
-    cy.fixture('dyson').then((data) => {
-        nbsHomepage.searchFor(data.dysonText);
-        nbsHomepage.selectDysonResult(data.dysonText);
-    });
+Given("I navigate to the Dyson manufacturer homepage", () => {
+  cy.fixture("urls").then((urls) => {
+    // tell cypress to use our url.json fixture file
+    nbsHomepage.visitURL(urls.nbsHomepage); // and use the nbsHomepage value in the visitURL function
+  });
+  nbsHomepage.acceptCookies();
+  cy.fixture("dyson").then((data) => {
+    // tell cypress to use our dyson.json fixture file
+    nbsHomepage.searchFor(data.dysonText); // and use the dysonText value in the searchFor function
+    nbsHomepage.selectDysonResult(data.dysonText); // and we can also use it when selecting our result from the search dropdown
+  });
 });
 
 // Then step to verify the URL contains expected text, the url is now passed in from the cucumber scenario step
-Then('The URL will contain the expected text {string}', (expectedUrlPart) => {
-    cy.fixture('dyson').then((dysonData) => {
-        dysonHomePage.verifyDysonPage(expectedUrlPart, dysonData.dysonText);//The search result we click on is now coming from the dysonHomepage.json fixture file
-    });
+Then("The URL will contain the expected text {string}", (expectedUrlPart) => {
+  dysonHomePage.verifyDysonPage(expectedUrlPart); 
 });
 
 // Then step to verify the telephone number is as expected
-Then('I verify telephone number is as expected', () => {
-    dysonHomePage.verifyContactNumber();
+Then("I verify telephone number is as expected", () => {
+  dysonHomePage.verifyContactNumber();
 });
 
 // Then step to verify the title on the page is as expected
-Then('I verify the title on the page is as expected', () => {
-    dysonHomePage.verifyTitle();
+Then("I verify the title on the page is as expected", () => {
+  dysonHomePage.verifyTitle();
 });
 
 // Then step to verify the href attribute of the Source logo is as expected
-Then('I verify the href attribute of the Source logo is as expected', () => {
-    dysonHomePage.verifySourceLogoHref();
+Then("I verify the href attribute of the Source logo is as expected", () => {
+  dysonHomePage.verifySourceLogoHref();
 });
 
 // Then step to Verify the external manufacturer link attribute contains the correct url
-Then('I verify the external manufacturer link attribute contains the correct url', () => {
+Then(
+  "I verify the external manufacturer link attribute contains the correct url",
+  () => {
     dysonHomePage.verifyWebsiteLink();
-});
+  }
+);
 
 // Then step to verify the contact manufacturer button shows the correct text
-Then('I verify the contact manufacturer button shows the correct text', () => {
-    dysonHomePage.verifyContactManufacturerButton();
-}); 
+Then("I verify the contact manufacturer button shows the correct text", () => {
+  dysonHomePage.verifyContactManufacturerButton();
+});
 
 //Then step to verify accessibility checks on the manufacturer homepage using AXE plugin and report results to console
-Then('I verify accessibility checks on the manufacturer homepage using AXE plugin and report results to console', () => {
+Then(
+  "I verify accessibility checks on the manufacturer homepage using AXE plugin and report results to console",
+  () => {
     dysonHomePage.verifyAccessibilityChecks();
-}); 
+  }
+);
 
 //Then step to verify the API response and content are as expected
-Then('I verify the API response and content are as expected', () => {
-    dysonHomePage.verifyApiResponse();
+Then("I verify the API response and content are as expected", () => {
+  dysonHomePage.verifyApiResponse();
 });
 
 // Then step to verify the Dyson navigation bar has the correct tabs and expected links
-Then('I verify the Dyson navigation bar has the correct tabs and expected links', () => {
+Then(
+  "I verify the Dyson navigation bar has the correct tabs and expected links",
+  () => {
     dysonHomePage.verifyDysonNavigationBar();
-});
-
-
-
+  }
+);
